9148eca3c0435661b1932338865cc7df0a36509f
Dina Zhyliaieva <din.zhyliaieva@gmail.com>
2025-08-07 19:48:46 +0300

    Fix issues

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,13 +1,21 @@
-export const Person = ({ person }) => (
-  <div className="Person">
-    <h2 className="Person__name">My name is {person.name}</h2>
-    {person.age && <p className="Person__age">I am {person.age}</p>}
-    {person.isMarried ? (
-      <p className="Person__partner">
-        {person.partnerName} is my {person.sex === 'f' ? 'husband' : 'wife'}
-      </p>
-    ) : (
-      <p className="Person__partner">I am not married</p>
-    )}
-  </div>
-);
+export const Person = ({ person }) => {
+  const { name, age, sex, isMarried, partnerName } = person;
+
+  return (
+    <div className="Person">
+      <h2 className="Person__name">My name is {name}</h2>
+      {person.age && <p className="Person__age">I am {age}</p>}
+      {isMarried ? (
+        <p className="Person__partner">
+          {partnerName} is my { sex === 'f'
+                                  ? 'husband'
+                                  : 'wife'
+          }
+
+        </p>
+      ) : (
+        <p className="Person__partner">I am not married</p>
+      )}
+    </div>
+  );
+};
